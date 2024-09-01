a7907e7c8111b6b0141424817650be335bad9066
amjadaj26 <79038026+amjadaj26@users.noreply.github.com>
2024-09-02 00:34:01 +0530

    Create Grading-Students.py

---
+++ MountBlue-Challenge/Grading-Students.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    my_result = []
+    for i in grades:
+        if(((i+1)%5 == 0) and i >= 38):
+            my_result.append(i+1)
+        elif(((i+2)%5 == 0) and i >= 38):
+            my_result.append(i+2)
+        else:
+            my_result.append(i)
+    return my_result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
