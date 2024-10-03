9f11f716c019fd14d90c300ecb6893203d3a4ec9
amjadaj26 <79038026+amjadaj26@users.noreply.github.com>
2024-10-04 00:03:01 +0530

    Create Time-Conversion.py

---
+++ MountBlue-Challenge/Time-Conversion.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from datetime import datetime
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    time_obj = datetime.strptime(s,'%I:%M:%S%p')
+    time_24hr = time_obj.strftime('%H:%M:%S')
+    return time_24hr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
